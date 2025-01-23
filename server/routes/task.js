3fc6f114981e8041115d098d4437d19f5165fb9d
LuckySibuyi <lucky1sibuyi@gmail.com>
2025-01-23 21:55:58 +0200

    delete redundant files

--- server/routes/task.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const taskController = require('../controllers/taskController');
-
-exports.homepage = async (req, res) => {
-   res.render('index');
-}
